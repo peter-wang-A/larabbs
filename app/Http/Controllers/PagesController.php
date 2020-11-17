f8fe32197218237a2d70bc7acc72d8d361aaa314
wyl <444079353@qq.com>
2020-11-17 14:36:13 +0800

    用户验证码

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
@@ -9,5 +10,19 @@ class PagesController extends Controller
     public function root()
     {
         return view('pages.root');
+    }
+
+
+    //注册验证
+    protected function validator(array $data){
+        return Validator::make($data,[
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'captcha' => ['required', 'captcha'],
+        ],[
+        'captcha.required' => '验证码不能为空',
+            'captcha.captcha' => '请输入正确的验证码',
+        ]);
     }
 }
