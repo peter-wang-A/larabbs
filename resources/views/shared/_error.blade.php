ff863b81efb6d9fd3b62c242a97de106050deec2
wyl <444079353@qq.com>
2020-11-18 11:05:38 +0800

    限制头像分辨率 ‘

--- resources/views/shared/_error.blade.php
+++ resources/views/shared/_error.blade.php
@@ -1,6 +1,5 @@
 @if (count($errors) > 0)
 <div class="alert alert-danger">
-  <div class="mt-2"><b>错误:</b></div>
   <ul class="mt-2 mb-2">
     @foreach ($errors->all() as $error)
     <li><i class="glyphicon glyphicon-remove"></i> {{ $error }}</li>
