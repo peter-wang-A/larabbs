3ad7064e89bcabb60cc2d515616a095433c533fe
wyl <444079353@qq.com>
2020-11-19 15:53:58 +0800

    翻译 URL 标士

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,9 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 //分类
 
