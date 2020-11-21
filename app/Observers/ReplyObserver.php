0f22cbb5ab95b54096b7d9bf36d101125b0729a9
vagrant <vagrant@homestead>
2020-11-21 04:18:48 +0000

    php artisan migrate:refresh --seed

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -13,12 +13,13 @@ class ReplyObserver
 {
     public function created(Reply $reply)
     {
-        //计算评论总和
-        $reply->topic->updateReplyCount();
+        if (!app()->runningInConsole()) {
+            //计算评论总和
+            $reply->topic->updateReplyCount();
 
-        //通知话题作者有新的评论
-         $reply->topic->user->notify(new TopicReplied($reply));
-
+            //通知话题作者有新的评论
+            $reply->topic->user->notify(new TopicReplied($reply));
+        }
     }
     public function saving(Reply $reply)
     {
