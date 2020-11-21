15b8b8e3bc6e5f37203a4bc2826a3241e908d118
wyl <444079353@qq.com>
2020-11-21 15:30:38 +0800

    用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
-
+use Illuminate\Support\Str;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -66,9 +66,34 @@ class User extends Authenticatable implements MustVerifyEmailContract
     }
 
     //清除通知
-    public function markAsRead(){
+    public function markAsRead()
+    {
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+    // 后台修改密码，修改器，当密码不等于60位时加密
+
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    // 后台修改头像拼接字符串
+    public function setAvatarAttribute($path)
+    {
+        if (!\Str::startsWith($path, 'http')) {
+            //拼接完整路径
+            $path = config('app.url') . '/uploads/images/avatars/'.$path;
+        }
+        return $this->attributes['avatar'] = $path;
     }
 }
